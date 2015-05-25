e11c6e2e29e5b0c1225c9973c7f95ff4312e42d7
linleague <labmarina@yandex.ru>
2015-05-25 21:42:20 +0300

    HW16+

--- addressbook-java-tests/src/com/example/fw/FolderHelper.java
+++ addressbook-java-tests/src/com/example/fw/FolderHelper.java
@@ -3,21 +3,20 @@ package com.example.fw;
 import java.util.ArrayList;
 import java.util.List;
 
-import javax.swing.JDialog;
+import org.netbeans.jemmy.operators.JButtonOperator;
+import org.netbeans.jemmy.operators.JDialogOperator;
+import org.netbeans.jemmy.operators.JTextFieldOperator;
+import org.netbeans.jemmy.operators.JTreeOperator;
 
-import org.netbeans.jemmy.operators.*;
-
-public class FolderHelper {
+public class FolderHelper extends HelpersBase {
 	
-	private ApplicationManager manager;
-
 	public FolderHelper(ApplicationManager app) {
-		this.manager = app;
+		super(app);
 	}
 
 	public Folders getFolders() {
 		List<String> list = new ArrayList<String>();
-		JTreeOperator tree = new JTreeOperator(manager.getApplication());
+		JTreeOperator tree = new JTreeOperator(mainFrame);
 		Object[] children = tree.getChildren(tree.getRoot());
 		for (Object child : children) {
 			list.add("" + child);
@@ -27,31 +26,10 @@ public class FolderHelper {
 
 	public String createFolder(String folderName) {
 		manager.getMenuHelper().pushCreateFolder();
-		JDialogOperator dialog = new JDialogOperator(manager.getApplication());
+		JDialogOperator dialog = new JDialogOperator(mainFrame);
 		new JTextFieldOperator(dialog).setText(folderName);
 		new JButtonOperator(dialog, "OK").push();
 		return waitMessageDialog("Warning", 3000);
-	}
-
-	private String waitMessageDialog(String title, int timeout) {
-		long start = System.currentTimeMillis();
-		long currentTime = start;
-		while (currentTime < start + timeout){
-			JDialog dialog = JDialogOperator.findJDialog(manager.getApplication().getOwner(), title, false, false);
-			if (dialog != null) {
-				JDialogOperator dialogOp = new JDialogOperator(dialog);
-				String message = new JLabelOperator(dialogOp).getText();
-				dialogOp.requestClose();
-				return message;
-			}
-			try {
-				Thread.sleep(500);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-			currentTime = System.currentTimeMillis();
-		}
-		return null;
 	}
 
 }
